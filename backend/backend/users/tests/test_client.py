1f2f347608502bc83ef4be2e1131294f7ea32446
Chris Adams <chris@productscience.co.uk>
2020-07-10 19:16:31 +0200

    Remove unneeded tests
    We use the passwordless approach for tokens now

--- backend/backend/users/tests/test_client.py
+++ backend/backend/users/tests/test_client.py
@@ -1,78 +1,23 @@
-import requests
-from rest_framework.test import RequestsClient, APIClient
 import pytest
 from rest_framework.authtoken.models import Token
-
-# import logging
-# console = logging.StreamHandler()
-# console.setLevel(logging.DEBUG)
-# logger = logging.getLogger(__file__)
-# logger.addHandler(console)
-# logger.setLevel(logging.DEBUG)
+from rest_framework.test import APIClient
 
 pytestmark = pytest.mark.django_db
 
 
-@pytest.mark.skip(
-    reason="These tests are triggering an exception, but the code is working when tested menually"
-)
-class TestIntegrationTestForCRUD:
-    def test_auth_to_get_token(self, profile, client):
-
-        # set password for user
-        profile.user.set_password("topsecret")
-        payload = {"username": profile.user.username, "password": "topsecret"}
-
-        # i.e. http://somesite.com/
-        base_url = ""
-        response = client.post(f"{base_url}auth-token/", payload)
-
-        data = response.json()
-        token = data.get("token")
-
-        token_from_db = Token.objects.first()
-
-        assert token == token_from_db.key
-
-
-@pytest.mark.only
 class TestIntegrationTestToAddProfilePhoto:
-
     def test_add_photo(self, db, profile, tmp_pic_path):
 
-        requests_client = RequestsClient()
-
-        token = Token(
-            key="short-and-readable",
-            user=profile.user
-        )
+        token = Token(key="short-and-readable", user=profile.user)
         token.save()
 
         client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
-        res = client.get("http://testserver/api/profiles/")
+        client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
 
-        assert res.status_code == 200
-
-        payload = {
-            'photo': open(tmp_pic_path, 'rb'),
-            'id': profile.id
-        }
+        payload = {"photo": open(tmp_pic_path, "rb"), "id": profile.id}
 
         res = client.put(f"http://testserver/api/upload/{profile.id}/", payload)
 
         assert res.status_code == 200
-
-        # requests_client.get("http://testserver/api/profiles",
-        #     headers={"Authorization": f"Token {token}"}
-        # )
-
-        # import ipdb ; ipdb.set_trace()
-
-        # token_from_db = Token.objects.first()
-
-        #  response = requests_client.get("http://testserver/users/api/profiles/", headers={"Authorization": "Token a0c22cbb9d95930d31e5d75af3affe8073e78b52" }, files={ 'file': pic })
-
-
-        # response = requests.requests_client("http://localhost:8000/api/upload/66/", headers={"Authorization": "Token a0c22cbb9d95930d31e5d75af3affe8073e78b52" }, files={ 'file': pic })
-
+        # check that we have the absolute URL in the payload
+        assert "http://testserver/" in res.data["photo"]
